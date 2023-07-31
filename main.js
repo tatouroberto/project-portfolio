142ed3bdf63056956207499a430e16ea173fc7d4
Reconfort Daniel <75314029+Reconfort@users.noreply.github.com>
2023-08-01 00:23:39 +0200

    Update main.js
    Add Hamburger Javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+const MenuSection = document.getElementById("Menu");
+
+const showMenu = () => {
+  MenuSection.style.display = "block";
+};
+const hideMenu = () => {
+  MenuSection.style.display = "none";
+};
