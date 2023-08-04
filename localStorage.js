0ed4332520aa0e8fc6b05aab366ded3a53e99c46
Reconfort Daniel <75314029+Reconfort@users.noreply.github.com>
2023-08-04 21:52:22 +0200

    Update localStorage.js
    Add local storage files

--- localStorage.js
+++ localStorage.js
@@ -8,4 +8,17 @@ const saveFormData = () => {
     // Save the entire form data as a single object in local storage
     localStorage.setItem("formData", JSON.stringify(formData));
   };
-  
+
+/ Function to load form data from local storage and pre-fill the form
+  const loadFormData = () => {
+    const formData = JSON.parse(localStorage.getItem("formData"));
+    if (formData) {
+      document.getElementById("name").value = formData.name || "";
+      document.getElementById("mail").value = formData.email || "";
+      document.getElementById("comment").value = formData.message || "";
+    }
+  };
+  // Add event listeners to save data on input change and load data on page load
+  const contactform = document.querySelector(".contact-form");
+  contactform.addEventListener("input", saveFormData);
+  window.addEventListener("load", loadFormData);
